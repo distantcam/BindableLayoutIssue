bfc715800e5ca898036fbbf3d9dbb82314e09bc8
Cameron MacFarland <distantcam@gmail.com>
2020-05-05 14:39:24 +0800

    Don't need the converter

--- BindableLayoutIssue/BindableLayoutIssue/MainViewModel.cs
+++ BindableLayoutIssue/BindableLayoutIssue/MainViewModel.cs
@@ -19,11 +19,14 @@ namespace BindableLayoutIssue
         private void Item_SelectionChanged(object sender, System.EventArgs e)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedItems)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedVisible)));
         }
 
         public IEnumerable<Selectable<string>> Items { get; }
 
         public IEnumerable<Selectable<string>> SelectedItems => Items.Where(i => i.Selected).ToArray();
+
+        public bool SelectedVisible => Items.Any(i => i.Selected);
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
